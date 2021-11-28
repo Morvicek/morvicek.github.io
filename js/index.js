6ad1c345a226a3331487f9f66fda00215e34f000
Morvicek <65585242+Morvicek@users.noreply.github.com>
2021-11-28 14:24:28 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -5,13 +5,15 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('cropy').innerText = `© 2019 - ${new Date().getFullYear()} ~ Morvicek`;
 
 })
+if(!window.location.pathname === "/"){
 document.addEventListener('click', musicplay);
-
+}
 function musicplay() {
 
     setTimeout(()=>{
         var audio = new Audio('./songs/downwitfsikness.mp3');
         audio.play();
+        audio.volume = "1000"
     },500)
 
   }
